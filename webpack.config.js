7963e70fcdadf42bc3afebf3ebc3552b8db82dc1
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-05-07 02:29:04 -0500

    Creación de alias para los componentes

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx'],
 		alias: {
-			'@assets': path.resolve(__dirname, 'src/assets')
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@atoms': path.resolve(__dirname, 'src/components/atoms'),
+			'@molecules': path.resolve(__dirname, 'src/components/molecules'),
+			'@organisms': path.resolve(__dirname, 'src/components/organisms'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
 		}
 	},
 	mode: 'development',
