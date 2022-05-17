e65e80df3887e993c52d6ac0967c59f275603d86
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-05-16 23:02:42 -0500

    Se agregó un parámetro al componente Button para que pueda recibir funciones onClick

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -3,13 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Button.css";
 
 const Button = (props) => {
-   const { icon, text, style, size, type } = props;
+   const { icon, text, style, size, type, handleClick } = props;
    return (
-      <button className={`Button ${style} ${size}`} type={type}>
+      <button className={`Button ${style} ${size}`} type={type} onClick={handleClick}>
          <FontAwesomeIcon icon={["fas", icon]} />
          {text}
       </button>
    );
 };
-
-export default Button;
